235f5338b05ea7fee8ca61a2f1045e5fdb89f845
Fabio Couto <fabio.couto@unicred.com.br>
2020-03-06 15:36:46 -0300

    criacao do post

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,9 @@ const port = 3000
 const express = require('express')
 const app = express()
 const foodRoutes = require('../routes/food-route')
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
 app.use('/', foodRoutes);
 
 app.listen(port, function(){
